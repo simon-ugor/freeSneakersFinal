d7ba8959922007a60fbda48eadf5ff9a8a5cc462
simon-ugor <simiugor@gmail.com>
2021-02-19 03:16:08 +0100

    style fix

--- pages/index.js
+++ pages/index.js
@@ -80,11 +80,11 @@ const index = () => {
                 <div style={{opacity: opacity}} className="main-howdoesitwork-div">
                     <h3 className="howdoesitwork-heading">How does it work?</h3>
                     <div className="howdoesitwork-text">
-                        <p>1.) Enter you email.</p>
-                        <p>2.) Click Generate button.</p>
+                        <p className="homepage-p">1.) Enter you email.</p>
+                        <p className="homepage-p">2.) Click Generate button.</p>
                         <p className="margin-bottom">3.) Your random number will be sent into our database.</p>
                         <p className="margin-bottom">Every week an email with the highest number wins free sneakers!</p>
-                        <p>You can generate new number every 5 hours. Simply repeat the
+                        <p className="homepage-p">You can generate new number every 5 hours. Simply repeat the
                             proccess and if you get higher number, it will be sent it!
                         </p>
                     </div>
