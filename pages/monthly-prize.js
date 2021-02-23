42c95e4f708de291ac0cc0bf67157c5b157ba641
simon-ugor <simiugor@gmail.com>
2021-02-23 02:11:55 +0100

    Metadata

--- pages/monthly-prize.js
+++ pages/monthly-prize.js
@@ -7,6 +7,10 @@ import Image from "next/image"
 const monthlyPrize = () => {
     return (
         <Layout>
+            <Head>
+                <title>FreeSneakers.xyz - Monthly Prize</title>
+                <meta name="description" content="Win free sneakers monthly just by entering your email! Try your luck and generate a random number. Highest number wins! Check out this months prize." />
+            </Head>
             <div className="aboutus-div">
                 <h3 className="aboutus-heading">Win this month!</h3>
                 <h3 className="aboutus-heading">02-2021</h3>
